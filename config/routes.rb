ecba9357dbed0bc91fbeaa8b299648467646781a
Unknown <jumcorredorro@unal.edu.co>
2017-09-23 00:54:08 -0500

    change routes to standard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :transactions
-  get '/byuserid', to: 'transactions#byUserId'
-  get '/bydate', to: 'transactions#byDate'
+  get '/by_user_id', to: 'transactions#byUserId'
+  get '/by_date', to: 'transactions#byDate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
