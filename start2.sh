9cba51ed61af4d86f5d4d1892caf6d05b36af0d7
Kevin Mendez <kmendezp@unal.edu.co>
2017-11-11 10:33:04 -0500

    fallara

---
+++ start2.sh
@@ -0,0 +1,8 @@
+./rancher-compose --project-name transactions-ms \
+--url http://192.168.99.100:8080/v1/projects/1a5 \
+--access-key 70EE95E3F0704CB1BC85 \
+--secret-key UbkJvYrYRBub9fsXxmzAP8GF2QVtmE4MsyeuFEBw \
+-f docker-compose.yml \
+--verbose up \
+-d --force-upgrade \
+--confirm-upgrade
